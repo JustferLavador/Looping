27508d98fd6f13c0dd5e96fc981b22ab9a179398
JustferLavador <118161608+JustferLavador@users.noreply.github.com>
2022-11-25 17:17:27 +0800

    Add files via upload

---
+++ newfile.py
@@ -0,0 +1,19 @@
+ctr = 2
+
+for ctr in range (100):
+	
+	ctr = ctr + 1
+	
+	if ctr == 50:
+		
+		print("50 is skipped")
+		
+		continue			#countinue here
+		
+		print ("this wont to be printed too,")
+		
+	print('number is: '	+ str(ctr))
+	
+print('out to loop')
+
+		
